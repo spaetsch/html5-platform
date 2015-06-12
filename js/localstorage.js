29bee3d5ef6632f1efaa588a4cdc8644a8633f67
Sarah Paetsch <spaetsch@gmail.com>
2015-06-11 18:07:24 -0700

    Clears local storage/input field on click of email_button

--- js/localstorage.js
+++ js/localstorage.js
@@ -2,33 +2,15 @@
 
 document.getElementById("email_from").addEventListener('keyup', function () {
   localStorage.setItem('store_email', this.value);
-  console.log(localStorage.store_email);
 });
 
+document.getElementById("email_button").addEventListener('click', function(){
+  localStorage.clear();
+});
 
-
-
-//on load puts it back
-/*$(document).ready(function () {
-    function init() {
-        if (localStorage["name"]) {
-            $('#name').val(localStorage["name"]);
-        }
-        if (localStorage["email"]) {
-            $('#email').val(localStorage["email"]);
-        }
-        if (localStorage["message"]) {
-            $('#message').val(localStorage["message"]);
-        }
-    }
-    init();
-});*/
 function init () {
   if (localStorage.store_email){
-    console.log("hello");
-    console.log(localStorage.store_email);
     document.getElementById("email_from").value = localStorage.store_email;
-
   }
 }
 
