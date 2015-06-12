728fec79cceddecb35c08a95440cc436cbcdffbd
Sarah Paetsch <spaetsch@gmail.com>
2015-06-11 17:57:32 -0700

    Email_from field storing and retrieving

--- js/localstorage.js
+++ js/localstorage.js
@@ -8,3 +8,28 @@ document.getElementById("email_from").addEventListener('keyup', function () {
 
 
 
+//on load puts it back
+/*$(document).ready(function () {
+    function init() {
+        if (localStorage["name"]) {
+            $('#name').val(localStorage["name"]);
+        }
+        if (localStorage["email"]) {
+            $('#email').val(localStorage["email"]);
+        }
+        if (localStorage["message"]) {
+            $('#message').val(localStorage["message"]);
+        }
+    }
+    init();
+});*/
+function init () {
+  if (localStorage.store_email){
+    console.log("hello");
+    console.log(localStorage.store_email);
+    document.getElementById("email_from").value = localStorage.store_email;
+
+  }
+}
+
+document.addEventListener('load', init());
